c64a60c85269d1aa89327953b6b813e0bfb1e8b0
Usman Mahmood <2142551+usmanmahmood@users.noreply.github.com>
2025-08-22 06:34:53 +0500

    Updated UserService

--- Maktab.Core/Interfaces/Services/IUserService.cs
+++ Maktab.Core/Interfaces/Services/IUserService.cs
@@ -6,6 +6,7 @@ namespace Maktab.Core.Interfaces.Services
      public interface IUserService : IDomainService
      {
           Task<UserInformationResponse> GetUserById(Guid id);
+          Task<bool> SendUserActivationCode(Guid userId);
           Task<bool> ValidateUserByActivationCode(Guid userId, UserVerificationRequest request);
      }
 }
--- Maktab.Domain/Services/UserService.cs
+++ Maktab.Domain/Services/UserService.cs
@@ -8,6 +8,8 @@ namespace Maktab.Domain.Services
      {
           private const string getUserById = @"/api/users/{0}";
           private const string validateUserActivationCode = @"/api/users/{0}/verify";
+          private const string sendActivationCode = @"/api/users/{0}/activationcode";
+
 
 
           public UserService(IHttpService httpService, ILocalStorageService localStorageService) 
@@ -26,6 +28,13 @@ namespace Maktab.Domain.Services
           {
                var formatedUrl = string.Format(validateUserActivationCode, userId);
                var result = await _httpService.Get<bool>(formatedUrl);
+               return result;
+          }
+
+          public async Task<bool> SendUserActivationCode(Guid userId)
+          {
+               var formatedUrl = string.Format(sendActivationCode, userId);
+               var result = await _httpService.Put<bool>(formatedUrl, null);
                return result;
           }
      }
