73b57ec0eb9213fcfee251671f0ea505d9ed69a2
Usman Mahmood <2142551+usmanmahmood@users.noreply.github.com>
2025-09-07 01:25:50 +0500

    Updated views

---
+++ Maktab.Consumer/Helpers/Constants.cs
@@ -0,0 +1,11 @@
+﻿namespace Maktab.Consumer.Helpers
+{
+     public static class Constants
+     {
+          public const string LoginRoute = "/account/login";
+          public const string CustomerDashboardRoute = "/customer/dashboard";
+          public const string CustomerAddProfileDetailRoute = "/customer/adddetails";
+
+          
+     }
+}
