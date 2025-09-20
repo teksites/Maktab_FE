bf51ef7a8601b31ab4a98f8584bae2c9d363ded6
Usman Mahmood <2142551+usmanmahmood@users.noreply.github.com>
2025-09-21 03:26:54 +0500

    Added contact service

--- Maktab.Core/Interfaces/Services/IContactService.cs
+++ Maktab.Core/Interfaces/Services/IContactService.cs
@@ -1,6 +1,15 @@
-﻿namespace Maktab.Core.Interfaces.Services
+﻿using MaktabDataContracts.Requests.OtherContacts;
+using MaktabDataContracts.Responses.OtherContacts;
+
+namespace Maktab.Core.Interfaces.Services
 {
      public interface IContactService : IDomainService
      {
+          Task<bool> DeleteContactByFamilyId(Guid familyId);
+          Task<bool> DeleteContactById(Guid contactId);
+          Task<OtherContactResponse> GetContactByFamilyId(Guid familyId);
+          Task<OtherContactResponse> GetContactById(Guid contactId);
+          Task<bool> HasContactAddedForFamily(Guid familyId);
+          Task<OtherContactResponse> SaveContactAsync(AddOtherContact contact);
      }
 }
--- Maktab.Domain/Services/ContactService.cs
+++ Maktab.Domain/Services/ContactService.cs
@@ -1,11 +1,70 @@
 ﻿using Maktab.Core.Interfaces.Services;
+using MaktabDataContracts.Requests.OtherContacts;
+using MaktabDataContracts.Responses.OtherContacts;
 
 namespace Maktab.Domain.Services
 {
      public class ContactService : BaseService, IContactService
      {
-          public ContactService(IHttpService httpService, ILocalStorageService localStorageService) : base(httpService, localStorageService)
+
+          private const string getContactById = @"/api/otherContacts/{0}";
+          private const string getContactByFamilyId = @"/api/families/{0}/othercontacts";
+          private const string saveContact = @"/api/families/{0}/othercontacts/add";
+          private const string updateAddress = @"/api/othercontacts/update";
+          private const string deleteContactById = @"/api/othercontacts/{0}/delete";
+          private const string deleteContactByFamilId = @"/api/families/{0}/othercontacts/delete";
+          private const string checkContactByFamilId = @"/api/families/{0}/othercontacts/check";
+
+          public ContactService(IHttpService httpService, ILocalStorageService localStorageService) 
+          : base(httpService, localStorageService)
           {
+          }
+
+          public async Task<OtherContactResponse> GetContactById(Guid contactId)
+          {
+               var formatedUrl = string.Format(getContactById, contactId);
+               var result = await _httpService.Get<OtherContactResponse>(formatedUrl);
+               return result;
+          }
+
+          public async Task<OtherContactResponse> GetContactByFamilyId(Guid familyId)
+          {
+               var formatedUrl = string.Format(getContactByFamilyId, familyId);
+               var result = await _httpService.Get<OtherContactResponse>(formatedUrl);
+               return result;
+          }
+
+          public async Task<OtherContactResponse> SaveContactAsync(AddOtherContact contact)
+          {
+               var result = await _httpService.Post<OtherContactResponse>(saveContact, contact);
+               return result;
+          }
+
+          //public async Task<AddressResponse> UpdateContact(AddAddress addAddress)
+          //{
+          //     var result = await _httpService.Post<AddressResponse>(updateAddress, addAddress);
+          //     return result;
+          //}
+
+          public async Task<bool> DeleteContactById(Guid contactId)
+          {
+               var formatedUrl = string.Format(deleteContactById, contactId);
+               var result = await _httpService.Post<bool>(formatedUrl, null);
+               return result;
+          }
+
+          public async Task<bool> DeleteContactByFamilyId(Guid familyId)
+          {
+               var formatedUrl = string.Format(deleteContactByFamilId, familyId);
+               var result = await _httpService.Post<bool>(formatedUrl, null);
+               return result;
+          }
+
+          public async Task<bool> HasContactAddedForFamily(Guid familyId)
+          {
+               var formatedUrl = string.Format(checkContactByFamilId, familyId);
+               var result = await _httpService.Post<bool>(formatedUrl, null);
+               return result;
           }
      }
 }
