611b67a58dcd5efb63df1d33d9de6e9217177ca1
Usman Mahmood <2142551+usmanmahmood@users.noreply.github.com>
2025-11-19 05:42:47 +0500

    Updated BaseDialog component also added BaseDialogWithButton component

--- Maktab.Consumer/Base/BaseDialog.cs
+++ Maktab.Consumer/Base/BaseDialog.cs
@@ -5,32 +5,6 @@ namespace Maktab.Consumer.Base
 {
      public class BaseDialog<T> : BaseComponent<T> where T : class
      {
-          [CascadingParameter]
-          protected IMudDialogInstance MudDialog { get; set; } = default!;
-
-          [Parameter] public RenderFragment? DialogBody { get; set; }
-
-          [Parameter] public EventCallback OnSave { get; set; }
-          [Parameter] public EventCallback OnCancel { get; set; }
-
-          [Parameter] public string SaveText { get; set; } = "Save";
-          [Parameter] public string CancelText { get; set; } = "Cancel";
-          [Parameter] public bool ShowActions { get; set; } = true;
-
-          protected virtual async Task OnSaveClickedAsync()
-          {
-               if (OnSave.HasDelegate)
-                    await OnSave.InvokeAsync();
-
-               MudDialog.Close(DialogResult.Ok(true));
-          }
-
-          protected virtual async Task OnCancelClickedAsync()
-          {
-               if (OnCancel.HasDelegate)
-                    await OnCancel.InvokeAsync();
-
-               MudDialog.Cancel();
-          }
+          [CascadingParameter] protected IMudDialogInstance MudDialog { get; set; } = default!;
      }
 }
---
+++ Maktab.Consumer/Base/BaseDialogWithButtons.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace Maktab.Consumer.Base
+{
+     public class BaseDialogWithButtons<T> : BaseDialog<T> where T : class
+     {
+          [Parameter] public RenderFragment? DialogBody { get; set; }
+
+          [Parameter] public EventCallback OnSave { get; set; }
+          [Parameter] public EventCallback OnCancel { get; set; }
+
+          [Parameter] public string SaveText { get; set; } = "Save";
+          [Parameter] public string CancelText { get; set; } = "Cancel";
+          [Parameter] public bool ShowActions { get; set; } = true;
+
+          protected virtual async Task OnSaveClickedAsync()
+          {
+               if (OnSave.HasDelegate)
+                    await OnSave.InvokeAsync();
+
+               MudDialog.Close(DialogResult.Ok(true));
+          }
+
+          protected virtual async Task OnCancelClickedAsync()
+          {
+               if (OnCancel.HasDelegate)
+                    await OnCancel.InvokeAsync();
+
+               MudDialog.Cancel();
+          }
+     }
+}
