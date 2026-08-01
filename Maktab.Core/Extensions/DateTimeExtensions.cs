03930e147caaa8c377820f6e558d97300407da27
Usman Mahmood <2142551+usmanmahmood@users.noreply.github.com>
2026-08-02 04:44:51 +0500

    Update code to start comparing age in months

--- Maktab.Core/Extensions/DateTimeExtensions.cs
+++ Maktab.Core/Extensions/DateTimeExtensions.cs
@@ -2,12 +2,17 @@
 {
      public static class DateTimeExtensions
      {
-          public static int GetAge(this DateTime dateOfBirth)
+          public static int GetAgeInYearsInt(this DateTime dateOfBirth)
+          {
+               return GetAgeInYearsInt(dateOfBirth, DateTime.Today);
+          }
+
+          public static int GetAgeInYearsInt(this DateTime dateOfBirth, DateTime referenceDate)
           {
                int age = 0;
 
                // Get today's date
-               DateTime today = DateTime.Today;
+               DateTime today = referenceDate;
 
                // Calculate the age in years
                age = today.Year - dateOfBirth.Year;
@@ -19,6 +24,18 @@
                }
 
                return age;
+          }
+
+          public static float GetAgeInYearsFloat(this DateTime dateOfBirth, DateTime referenceDate)
+          {
+               //We need to get age in float number like 4.5 years old
+               var ageInDays = (referenceDate - dateOfBirth).TotalDays;
+               return (float)ageInDays / 365.25f;
+          }
+
+          public static float GetAgeInMonths(this DateTime dateOfBirth, DateTime referenceDate)
+          {
+               return GetAgeInYearsFloat(dateOfBirth, referenceDate) * 12;
           }
      }
 }
