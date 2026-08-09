d6e70c8b540c757283da0b3e5f8aae39de2d4e3d
Usman Mahmood <2142551+usmanmahmood@users.noreply.github.com>
2026-08-10 00:42:23 +0500

    Improved the visuals of age restriction for course group in enrollment dialog

--- Maktab.Core/Extensions/IntegerExtensions.cs
+++ Maktab.Core/Extensions/IntegerExtensions.cs
@@ -7,6 +7,34 @@
                return ageInMonths / 12.0f;
           }
 
+          public static string MonthToYearsInString(this int ageInMonths)
+          {
+               if (ageInMonths > 0)
+               {
+                    int years = ageInMonths / 12;   // Integer division gives full years
+                    int months = ageInMonths % 12;  // Remainder gives leftover months
+
+                    if (years > 0)
+                    {
+                         if (months > 0)
+                         {
+                              return $"{years}y {months}m";
+                         }
+                         else
+                         {
+                              return $"{years}y";
+                         }
+                    }
+
+                    if (months > 0)
+                    {
+                         return $"{months}m";
+                    }
+               }
+
+               return string.Empty;
+          }
+
           public static int MonthToYearsAdjustedMidPoint(this int ageInMonths)
           {
                var years = MonthToYears(ageInMonths);
