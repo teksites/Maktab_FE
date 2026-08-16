35b2a5707d528354c4b06c8f8bf28b153fcbf79e
Usman Mahmood <2142551+usmanmahmood@users.noreply.github.com>
2026-08-16 19:15:41 +0500

    Revert "Removed the changes for UI Theming"
    This reverts commit 2b825e2be4b204352fec29cbe434fc7111d51ed6.

--- Maktab.Consumer/Theme/AppTheme.cs
+++ Maktab.Consumer/Theme/AppTheme.cs
@@ -8,19 +8,6 @@
           {
                PaletteLight = new PaletteLight()
                {
-                    Primary = Colors.BlueGray.Darken2,
-               },
-               PaletteDark = new PaletteDark()
-               {
-                    Primary = Colors.BlueGray.Lighten1
-               },
-          };
-
-          /*
-          public static MudTheme IccBrossardTheme => new MudTheme
-          {
-               PaletteLight = new PaletteLight()
-               {
                     Primary = "#1A5C50",
                     PrimaryDarken = "#1F6B5D",
                     PrimaryLighten = "#B3DDD5",
@@ -125,6 +112,5 @@
                     AppbarHeight = "60px",
                },
           };
-          */
      }
 }
